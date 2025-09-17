010f0e19a1590c3fd7f184a491538b9f3c3d9766
Manohar Kale <manoharkale5368@gmail.com>
2025-09-17 17:17:16 +0530

    vercel config change

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import app from "./app";
 
-// const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT ?? 5000;
 
-// app.listen(PORT, () => {
-//   console.log(`PromptFlow backend listening on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`PromptFlow backend listening on port ${PORT}`);
+});
