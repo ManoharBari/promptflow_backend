15f3bb2594c3c3e20d424c327f9daecee1e63481
Manohar Kale <manoharkale5368@gmail.com>
2025-10-02 18:07:24 +0530

    fix userid issue

--- prompt/prompt.service.ts
+++ prompt/prompt.service.ts
@@ -4,6 +4,11 @@ import { generateJsonFromPrompt } from "../gemini/gemini.client";
 export async function createPrompt(userId: string, inputText: string) {
   const outputJson = await generateJsonFromPrompt(inputText);
 
+  const user = await prisma.user.findUnique({ where: { clerkId: userId } });
+  if (!user) {
+    throw new Error(`User not found`);
+  }
+
   const prompt = await prisma.prompt.create({
     data: {
       userId: userId,
